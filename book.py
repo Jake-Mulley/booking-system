8585e45eae771a866c7bba4455a988e508aeb700
Jake Mulley <71042647+Jake-Mulley@users.noreply.github.com>
2021-11-09 18:43:40 +0000

    Update book.py
    changed from wrong file to right file

--- book.py
+++ book.py
@@ -1,12 +1,13 @@
 #!/usr/local/bin/python3
-
 from cgitb import enable
 
 enable()
 
 from cgi import FieldStorage
 from html import escape
+from hashlib import sha256
 from os import environ
+from time import time
 from shelve import open
 from http.cookies import SimpleCookie
 import pymysql as db
@@ -33,33 +34,40 @@ try:
             sid = cookie['sid'].value
             session_store = open('sess_' + sid, writeback=False)
             if session_store.get('authenticated'):
-                connection = db.connect('localhost', '', '', '')
-                cursor = connection.cursor(db.cursors.DictCursor)
+                username = session_store['username']
 
-                cursor.execute('SELECT * FROM users ORDER BY highScore DESC')
-                table = '<table><tr><th>Place</th><th>Name</th><th>Highscore</th></tr>'
-                counter = 0
-                for row in cursor.fetchall():
-                    counter += 1
-                    table += '<tr><td>%s</td><td>%s</td><td>%s</td></tr>' % (
-                    str(counter), row['username'], row['highScore'])
-                table += '</table>'
-                cursor.close()
-                connection.close()
-                result = """
-                    <nav>
-                            <a href=""></a>
-                            <a href=""> </a>
-                            <a href=""> </a>
-                            <a href="logout.py">Log Out </a>
-                    </nav>
-                    <main>
-                            <h1></h1>
-                            <p></p>
-                            %s
-                    </main>
-                    """ % (table)
-            session_store.close()
+                form_data = FieldStorage()
+                table = ''
+                time = ''
+                if len(form_data) != 0:
+                    table = escape(form_data.getfirst('table', '').strip())
+                    time = escape(form_data.getfirst('time', '').strip())
+
+                    if not table or not time:
+                        # User did not put in required information
+                        result = '<p>Table Number and time are required</p>'
+
+                    else:
+                        try:
+                            connection = db.connect('localhost', 'jm72', 'phoht', 'cs6503_cs1106_jm72')
+                            cursor = connection.cursor(db.cursors.DictCursor)
+                            cursor.execute("""SELECT * FROM bookings 
+                                              WHERE TableID = %s AND BookingTime = %s""", (table, time))
+                            if cursor.rowcount > 0:
+                                result = '<p>That table has already taken for that time</p>'
+                            else:
+                                # create booking
+
+                                cursor.execute("""INSERT INTO bookings (Person, BookingTime, TableID)
+                                                  VALUES (%s, %s, %s)""", (username, time, table))
+                                connection.commit()
+                                cursor.close()
+                                connection.close()
+
+                        except (db.Error, IOError):
+                            # nts, improve output here
+                            result = '<p>Sorry! We are experiencing problems at the moment. Please call back later. </p>'
+
 except IOError:
     result = '<p>Sorry! We are experiencing problems at the moment. Please call back later.</p>'
 
@@ -68,19 +76,31 @@ print("""
     <html lang="en">
         <head>
             <meta charset="utf-8" />
-            <title>Reservation System &#124; View Bookings</title>
+            <title>Reservation System &#124; register</title>
             <link rel="stylesheet" href="styles.css">
         </head>
         <body>
             <header>
                 <h1>Reservation System</h1>
             </header>
+            <main>
+                <h1>Register:</h1>
+                <form action="test.py" method="post">
+                    <label for="table">Table Number: </label>
+                    <input type="text" name="table" id="table" value="%s" />
+                    <label for="time">Time (format: YYYY-MM-DD hh:mm:ss): </label>
+                    <input type="text" name="time" id="time" />
 
-            %s
-            <aside></aside>
+                    <input type="submit" value="Book" />
+                </form>
+                <p>Use the above boxes to enter a valid table number and the time with which you wish to book</p>
+                <p><a href="login.py"> Log in</a></p>
+
+                %s
+            </main>
             <footer>
-                <small>&copy; Group 3 CS3500 2021</small>
+                <small>&copy; Group 3 3500 2021</small>
                 <a href="#header">Back To The Top</a>
-	        </footer>
+            </footer>
         </body>
-    </html>""" % (result))
+    </html>""" % (table, result))
